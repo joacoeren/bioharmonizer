6499aa85a039b39e0a0e592160e80bd4cc62424e
Joaco <joacoeren@gmail.com>
2025-11-16 23:45:17 -0300

    cambior

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="app">
       <div className="app-container">
-        <h1 className="app-title">Mi Bio-Harmonizer</h1>
         <PhoneMockup />
       </div>
     </div>
